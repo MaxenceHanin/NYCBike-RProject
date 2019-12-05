a696d8719158dc700e1f92ae510d9e2bed6ad4b7
MaxenceHanin <hanin@etud.insa-toulouse.fr>
2019-12-05 23:08:34 +0100

    Add number of bike by weekday and hour

--- Script2.R
+++ Script2.R
@@ -3,8 +3,9 @@ library(dplyr)
 library(ggplot2)
 library(data.table)
 library(ggmap)
+library(lubridate)
 
-setwd("~/Documents/INSA/5A/AnaDesc/NYCBike-RProject/")
+setwd("~/Documents/5e_annee/NYCBike-RProject/")
 data <- list()
 
 # Import datasets and join them in one dataframe
@@ -53,7 +54,17 @@ most_freq = merge(most_freq, end, by.x = "station.id", by.y = "end.station.id")
 nymap = get_map(location=c(-74.0, 40.7), maptype="terrain")
 ggmap(nymap) + geom_point(aes(x = station.longitude, y = station.latitude, cex=start.count), data=most_freq) + xlim(-74.05, -73.9) + ylim(40.63, 40.82)
 
-# Nombre de trajets en fonction de l'heure de la journée
+# Nombre de trajets en fonction de l'heure de la journée / le jour de la semaine
+data$startdate = parse_date_time(data[,"starttime"][[1]], "%Y-%m-%d %H:%M:%OS")
+data$day = weekdays(data$startdate)
+data$hour = hour(data$startdate)
 
+by_day = data[, .(day_count=.N), by= day]
+by_day$day = ordered(by_day$day, levels=c("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"))
+
+ggplot(by_day, aes(x = day, y = day_count)) + geom_bar(stat="identity")
+
+by_hour = data[, .(hour_count=.N), by= hour]
+ggplot(by_hour, aes(x = hour, y = hour_count)) + geom_line()
 
 # Afflux et variation de la proportion de vélos sur une station
