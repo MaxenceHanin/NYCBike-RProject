fa6e78f976abc32c08a356cd2a1a3709050177c6
Louis Jean <ljean@etud.insa-toulouse.fr>
2019-12-06 15:16:23 +0100

    Add cumulative sum of bike on one station

--- Script2.R
+++ Script2.R
@@ -4,8 +4,9 @@ library(ggplot2)
 library(data.table)
 library(ggmap)
 library(lubridate)
+library(rstudioapi)
 
-setwd("~/Documents/5e_annee/NYCBike-RProject/")
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 data <- list()
 
 # Import datasets and join them in one dataframe
@@ -49,13 +50,17 @@ start = data[, .(start.count=.N), by= start.station.id]
 end = data[, .(end.count=.N), by= end.station.id]
 most_freq = merge(names, start, by.x = "station.id", by.y = "start.station.id")
 most_freq = merge(most_freq, end, by.x = "station.id", by.y = "end.station.id")
+most_freq$frequentation = most_freq$start.count + most_freq$end.count
 
 # Plot it!
-nymap = get_map(location=c(-74.0, 40.7), maptype="terrain")
-ggmap(nymap) + geom_point(aes(x = station.longitude, y = station.latitude, cex=start.count), data=most_freq) + xlim(-74.05, -73.9) + ylim(40.63, 40.82)
+nymap = get_map(location=c(-74.05, 40.63, -73.9, 40.82), maptype="terrain", zoom = 13)
+ggmap(nymap) +
+  geom_point(aes(x = station.longitude, y = station.latitude, cex=frequentation), col = "#a31829", bg = "#fa7080", pch=21, data=most_freq) +
+  xlim(-74.05, -73.9) + ylim(40.63, 40.82)
 
 # Nombre de trajets en fonction de l'heure de la journée / le jour de la semaine
 data$startdate = parse_date_time(data[,"starttime"][[1]], "%Y-%m-%d %H:%M:%OS")
+data$enddate = parse_date_time(data[,"stoptime"][[1]], "%Y-%m-%d %H:%M:%OS")
 data$day = weekdays(data$startdate)
 data$hour = hour(data$startdate)
 
@@ -68,3 +73,17 @@ by_hour = data[, .(hour_count=.N), by= hour]
 ggplot(by_hour, aes(x = hour, y = hour_count)) + geom_line()
 
 # Afflux et variation de la proportion de vélos sur une station
+station = most_freq[order(-start.count)][1]$station.id
+input = data[end.station.id == station]
+input$value = 1
+input$date = input$startdate
+output = data[start.station.id == station]
+output$value = -1
+output$date = output$enddate
+
+trips = rbind(input, output)[order(date)] #[day(date) == 5]
+trips$cumul = cumsum(trips$value)
+trips$cumul = trips$cumul - min(trips$cumul)
+capacity = max(trips$cumul)
+
+ggplot(trips, aes(x = date, y = trips$cumul)) + geom_line()
